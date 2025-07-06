fdb671a290ff22793aa8fb1fc6e8b9aca6288e12
Pulkit Porwal <pulkitporwal.brn@gmail.com>
2025-07-06 13:31:37 +0530

    Categories Issue Resolved

--- app/(dashboard)/[storeId]/(routes)/categories/page.tsx
+++ app/(dashboard)/[storeId]/(routes)/categories/page.tsx
@@ -20,7 +20,7 @@ const CategoriesPage = async ({ params }: { params: { storeId: string } }) => {
   const formattedCategories: CategoryColumn[] = categories.map((item) => ({
     id: item.id,
     name: item.name,
-    billboardLabel: item.billboard.label,
+    billboardLabel: item?.billboard?.label ?? "No billboard",
     createdAt: format(item.createdAt, "MMMM do, yyyy"),
   }));
 
